f4ec8fb005b1db877087481c816fa671f7a1b105
khanfaryal55 <khanfaryal55@gmail.com>
2024-04-22 23:37:46 +0500

    first commit

---
+++ Assignment#3/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// storing the Famous quotesand its author //
+let quote = "Nothing is impossible beacause impossible itself i am possible";
+let author = "usman ghani";
+console.log(`"${quote}" - ${author}`);
---
+++ Assignment#3/main.ts
@@ -0,0 +1,9 @@
+// storing the Famous quotesand its author //
+
+let quote :String="Nothing is impossible beacause impossible itself i am possible";
+
+let author:String="usman ghani";
+
+// Printing the message//
+
+console.log(`"${quote}" - ${author}`);
